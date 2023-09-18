e6ad72e7fb19e344912bb017014bd811e1752791
Fugazzii <sichinavailia@gmail.com>
2023-09-18 22:31:54 +0400

    Added main description

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+/**
+ * This file has no direct purpose
+ * It just exists for cargo project to define entry main file
+ * In-memory database is run directly from the bin
+ */
+
 pub mod helper;
 
 pub fn main() {
-    println!("Hello, world!");
+    unimplemented!();
 }
